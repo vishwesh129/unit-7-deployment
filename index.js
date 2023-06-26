55a1bfa6f5c49f4a401b045b12227a683287e610
vishwesh129 <nakkalavishwesh@gmail.com>
2023-06-26 14:57:50 +0530

    basic express app

---
+++ index.js
@@ -0,0 +1,16 @@
+require("dotenv").config();
+const express = require('express');
+
+const app = express();
+
+app.get("/", (req, res) => {
+    res.send({"msg" : "Base of API"})
+})
+
+app.get("/blogs", (req, res) => {
+    res.send({"Blogs" : "blogs blogs blogs"})
+})
+
+app.listen(process.env.PORT, (req, res) => {
+    console.log(`listening on port localhost:${process.env.PORT}`)
+})
